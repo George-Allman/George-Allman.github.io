237a56fe40cdb529b3823aa3d1f2c7d2e23024d0
Mozsii <george.allman777@gmail.com>
2026-08-19 23:04:00 +1000

    deploy 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/George-Allman.github.io/', // must match your GitHub Pages URL path
+  base: '/', // must match your GitHub Pages URL path
   plugins: [react()],
 })
